df0d361247677dc1332fd31dd495a0e6f0d332ea
anurag <anurag642001@gmail.com>
2023-10-06 22:34:47 +0530

    first commit

--- js/app.js
+++ js/app.js
@@ -20,3 +20,26 @@ times.addEventListener('click', function(){
 });
 
 });
+
+function sendEmail(){
+  
+  // let sender_email=document.getElementById("sender_email").value;
+  // let sender_subject=document.getElementById("sender_subject").value;
+  // let sender_message=document.getElementById("sender_message").value;
+  let sender_email='Anurag@gmail.com';
+  let sender_subject='time pass';
+  let sender_message='sample body';
+  console.log(sender_email,sender_subject,sender_message);
+  event.preventDefault(); 
+    Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "gunjanatyourservice@gmail.com",
+      Password : "gygk heqy jfaa fjnw",
+      To : 'anurag642001@gmail.com',
+      From : sender_email,
+      Subject : sender_subject,
+      Body :sender_message
+  }).then(
+    message => alert(message)
+  );
+}
